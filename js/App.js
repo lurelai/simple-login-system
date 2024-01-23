1decbffd58671bd82d777c0eeca1c9906dd6a9cc
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-01-23 12:58:20 -0300

    Okay

--- js/App.js
+++ js/App.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res)=>{
-    res.send('Ola Mundo')
+    res.sendFile( path.join(__dirname, '../index.html') )
+})
+
+app.post('/form', (req, res)=>{
+    const { nome, senha } = req.body
+
+    if(nome === 'Lucas' && senha === 'senha')
+        res.send('okay')
+
+    else
+        res.send('not correct')
+
 })
 
 app.listen(8080)
